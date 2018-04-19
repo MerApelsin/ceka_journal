9aa2e6b566a2d45a85c85162981897faa65414af
MerApelsin <cecilia.carlsson93@gmail.com>
2018-04-19 22:05:53 +0200

    update workin'

--- partials/update_entry.php
+++ partials/update_entry.php
@@ -1,23 +1,24 @@
 <?php
     require_once 'database.php';
     
-    //header('Location: \partials\get_entries.php');
+    header('Location: \partials\get_entries.php');
     //Check and fetch the ID that's supposed to be updated
     if (isset($_GET["entryID"])) {
-        $thisID = (int)$_GET["entryID"];
+        $ID = (int)$_GET["entryID"];
     }
-    var_dump($thisID);
+    var_dump($ID);
     //Update that jazz
     $statement = $db->prepare(
         "UPDATE entries
         SET title = :title, 
-            content = :content,
+            content = :content
         WHERE entryID = :entryID"
     );
+
     $statement->execute([
         ":title" => $_POST["title"],
         ":content" => $_POST["content"],
-        ":entryID" => $thisID
+        ":entryID" => $ID
     ]);
 
 ?>
